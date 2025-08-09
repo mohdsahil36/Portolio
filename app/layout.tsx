25e051b31b9550ec1e1c19bd7145f111479af866
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2025-08-09 13:04:52 +0530

    unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Mono, Poppins, DM_Sans } from "next/font/google";
+import { IBM_Plex_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
